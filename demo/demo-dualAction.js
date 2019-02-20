d90264cb79a229238d96c34b32c90bb8ed9f9b68
Joe Bondra <bondra.4@osu.edu>
2019-02-20 13:55:24 -0500

    Adds demos for vertical/dualAction options.

---
+++ demo/demo-dualAction.js
@@ -0,0 +1,10 @@
+/**
+ * @file demo.js
+ */
+window.addEventListener("load", function(event) {
+  var options = {
+    'ariaLabel' : 'Main Navigation',
+    'mode' : 'dualAction'
+  }
+  var test = new a11yMenubar('main-nav', options);
+});
---
+++ demo/demo-vertical.js
@@ -0,0 +1,10 @@
+/**
+ * @file demo.js
+ */
+window.addEventListener("load", function(event) {
+  var options = {
+    'ariaLabel' : 'Main Navigation',
+    'ariaOrientation' : 'vertical'
+  }
+  var test = new a11yMenubar('main-nav', options);
+});
