abbe3820d94446ee3c8fc99418698899fab0e9bb
Joe Bondra <bondra.4@osu.edu>
2019-03-04 13:59:20 -0500

    Adjusts vertical demo script.

--- demo/js/demo-vertical.js
+++ demo/js/demo-vertical.js
@@ -4,7 +4,7 @@
 window.addEventListener("load", function(event) {
   var options = {
     'ariaLabel' : 'Main Navigation',
-    'ariaOrientation' : 'vertical'
+    'orientation' : 'vertical'
   }
   var test = new a11yMenubar('main-nav', options);
 });
